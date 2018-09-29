d074a35a61fe8aae151faffcc47d8c241c51f6f6
Lucile <lucile.lemiere@outlook.fr>
2018-09-29 21:56:34 +0200

    Initial commit

---
+++ homework0309.js
@@ -0,0 +1,26 @@
+fetch('http://api.citybik.es/v2/networks/bicloo').then(function(res) {
+	return res.json().then(function(data) {
+		var stations = data.network.stations;
+    //écriture ici
+for (var i = 0; i < stations.length; i++) {
+  console.log(stations[i].name + ' // ' + stations[i].empty_slots + ' // ' + stations[i].free_bikes); //à compléter/revoir);
+
+  var liste = document.querySelector('ul');
+  var currentLi = document.createElement('li');
+  currentLi.textContent = stations[i].empty_slots;
+  liste.appendChild(currentLi)
+}
+    //et là
+	});
+});
+
+// HOMEWORK :
+//AFFICHER POUR CHAQUE STATION LE NOMBRE DE VELOS DISPO empty_slots et free bikes
+// indicateur visuel lorsqu'il ya moins de 50% des vélos disponibles BONUS (système de proportion)
+// déduire un nombre de places grâces aux places vides et free-bikes
+
+//var liste = document.querySelector('ul');
+// for (var i = 0; i < etudiants.length; i++) {
+// 	var currentLi = document.createElement('li');
+// 	currentLi.textContent = etudiants[i];
+// 	liste.appendChild(currentLi);
